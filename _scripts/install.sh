dc01d20f1ffc302fb7e286f28dd4e08fd8a7ea7c
dipo Majekodunmi <dipo@dipolediamond.com>
2017-08-05 00:31:18 +0100

    testing travis commit

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -2,7 +2,7 @@
 set -x # Show the output of the following commands (useful for debugging)
     
 # Import the SSH deployment key
-- openssl aes-256-cbc -K $encrypted_0915310d3ea2_key -iv $encrypted_0915310d3ea2_iv -in lbpm-deploy.enc -out lbpm-deploy -d
+openssl aes-256-cbc -K $encrypted_0915310d3ea2_key -iv $encrypted_0915310d3ea2_iv -in lbpm-deploy.enc -out lbpm-deploy -d
 rm lbpm-deploy.enc # Don't need it anymore
 chmod 600 lbpm-deploy
 mv lbpm-deploy ~/.ssh/id_rsa
